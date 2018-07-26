3715b40a59889ef6c8f059b728a6944e53ff0a28
grantzhu <grantzhu@uniquesoft.com>
2018-07-26 20:18:43 +0800

    refresh chat page every 1 second, show last 5 chat messages of current date

--- chat.py
+++ chat.py
@@ -1,5 +1,8 @@
 
 from flask import Flask,render_template,session,request,redirect,url_for
+from flask_pymongo import PyMongo
+import datetime
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -7,6 +10,10 @@ app = Flask(__name__)
 app.debug = True
 #set session secret_key
 app.secret_key = 'any random string'
+
+#connect mongodb
+app.config["MONGO_URI"] = "mongodb://localhost:27017/chatonline"
+mongo=PyMongo(app)
 
 #set nickname page
 @app.route('/')
@@ -26,7 +33,25 @@ def setUsername():
 def getUsername():
     return session['username']
     
-
+#save message
+@app.route('/sendMessage',methods = ['POST', 'GET'])
+def saveMessage():
+    username = session['username']
+    #message = request.form['chatmessage']
+    message = request.json['message']
+    messageDate = datetime.datetime.today()
+    messageDate = messageDate.strftime("%Y-%m-%d")
+    mongo.db.chatmessage.save({'username':username,'message':message,'date':messageDate})
+    #return render_template('chat.html')
+    return 'success'
+#get message
+@app.route('/getMessage')
+def getMessage():
+    #find last 10 message today
+    todayDate = datetime.datetime.today().strftime("%Y-%m-%d")
+    #messageDir = mongo.db.chatmessage.find({'date':todayDate}).sort({_id:-1}).limit(5)
+    messageDir = mongo.db.chatmessage.find({'date':todayDate}).sort("_id",-1).limit(5)
+    return dumps(messageDir)
     
 if __name__ == '__main__':
     app.run()
--- static/chat.js
+++ static/chat.js
@@ -1,22 +1,76 @@
 
-//send message button click
-function sendMessage(){
+//get username
+function getUser(){
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-	  // messageContent=document.getElementById('message').innerHTML
-	  username=this.responseText
-	  inputMessage=document.getElementById("inputMessage").value
-	  chatMessage = username+': '+inputMessage
-	  var liNode = document.createElement("li");                 // Create a <li> node
-	  var textnode = document.createTextNode(chatMessage);         // Create a text node
-	  liNode.appendChild(textnode);                              // Append the text to <li>
-	  document.getElementById("message").appendChild(liNode);     // Append <li> to <ul> with id="myList"
-	  document.getElementById("inputMessage").value=''
+    xhttp.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200){
+	    username = this.responseText
+	    //get chat message
+	    getMessage(username)
+	}
+    };
+    
+    //set sync
+    xhttp.open("GET","/getUsername",true);
+    xhttp.send();
 
+}
+
+//get chat message
+function getMessage(username){
+    //delete all message
+    deleteALlMessage()
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+	//parse to JSON  
+	message = JSON.parse(this.responseText)
+	//trverse message directory
+	for (let i = message.length-1; i >= 0; i--){
+	    chatMessage = message[i].username+': '+message[i].message
+	    var liNode = document.createElement("li");                 // Create a <li> node
+	    var textnode = document.createTextNode(chatMessage);         // Create a text node
+	    liNode.appendChild(textnode);                              // Append the text to <li>
+	    document.getElementById("message").appendChild(liNode);     // Append <li> to <ul> with id="myList"
+
+	}
     }
   };
   //set sync
-  xhttp.open("GET", "/getUsername", true);
+  xhttp.open("GET", "/getMessage", true);
   xhttp.send();
 }
+
+//delete all message
+function deleteALlMessage(){
+    var messageDivNode =  document.getElementById("message");
+    while(messageDivNode.hasChildNodes())
+    {
+	messageDivNode.removeChild(messageDivNode.lastChild);
+    }
+}
+
+//send message
+function sendMessage(){
+    message = document.getElementById("inputMessage").value
+    //empty input message
+    document.getElementById("inputMessage").value=''
+    //send message
+    var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function(){
+    // 	if(this.readyState == 4 && this.status == 200){
+    // 	    //alert('send message success')
+    // 	}
+    // };
+    
+    //set sync
+    xhttp.open("POST","/sendMessage",true);
+    //send json data
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({"message":message}));    
+        
+}
+
+//set get user run every 2s
+setInterval('getUser()', 1000)
+//get User and message
